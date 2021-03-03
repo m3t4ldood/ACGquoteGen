07c3a3c124a6de9d2b3253d368cbeed925b677fc
Anthony Gannuscio <m3t4ldood@gmail.com>
2021-03-02 17:58:46 -0800

    added crud functionality

---
+++ src/controllers/controller.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+// Create and Save
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Quotes from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single quote with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a quote by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a quote with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all quote from the database.
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published quotes
+exports.findAllPublished = (req, res) => {
+
+};
